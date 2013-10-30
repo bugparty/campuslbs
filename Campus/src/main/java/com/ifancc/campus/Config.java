ca97068dcdaed6296527b7dc29957b7683686301
bowman <fancycode@gmail.com>
2013-10-30 14:16:46 +0800

    init commit

---
+++ Campus/src/main/java/com/ifancc/campus/Config.java
@@ -0,0 +1,9 @@
+package com.ifancc.campus;
+
+/**
+ * Created by bowman on 13-10-30.
+ * This is the global config file
+ */
+public class Config {
+    public static String AppName = "CampusLBS application";
+}
