48dccaf6466f120a067f592dd1d69641394f97d0
madman_fei <630431321@qq.com>
2013-12-18 15:35:43 +0800

    install baidu.lib

---
+++ Campus/src/main/java/com/ifancc/campus/Map/MapActivity.java
@@ -0,0 +1,15 @@
+package com.ifancc.campus.Map;
+
+import android.app.Activity;
+
+import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.map.MapView;
+
+/**
+ * Created by Administrator on 13-12-17.
+ */
+public class MapActivity extends Activity {
+   private BMapManager mBMapManager;
+   private MapView mMapView;
+
+}
